2737e7b17c69a6c74be245f2f8b9256b967417ac
xingmao317 <xingmao317@gmail.com>
2025-01-14 09:05:43 +0700

    Update libraries_6_1_2568.ino

--- libraries_6_1_2568.ino
+++ libraries_6_1_2568.ino
@@ -22,63 +22,6 @@ void loop() {
    modbusread();
    Eeprom(); 
  /////////////////////////////////////////////
-   if(DI1==1){DOut(1,1);}else{DOut(1,0);} //{DigitalOutput(channel,status)
-   if(DI2==1){DOut(2,1);}else{DOut(2,0);}
-   if(DI3==1){DOut(3,1);}else{DOut(3,0);}
-   if(DI4==1){DOut(4,1);}else{DOut(4,0);}
-   if(DI5==1){DOut(5,1);}else{DOut(5,0);}
-   if(DI6==1){DOut(6,1);}else{DOut(6,0);}
-   if(DI7==1){DOut(7,1);}else{DOut(7,0);}
-   if(DI8==1){DOut(8,1);}else{DOut(8,0);}
-
-   PIDAOut1(setpoint1,AI2,kp1,ki1,kd1,30,100);//kp1,ki1,kd1
-   //PIDAOut2(3000,AI2,kp1,ki1,kd1,30,100);
-   //PIDAOut3(3000,AI3,kp1,ki1,kd1,30,100);
-   //PIDAOut4(3000,AI4,kp1,ki1,kd1,30,100);
-   
-  /*Serial.print("AI1 ");
-  Serial.print(AI1);
-  Serial.print("AI2 ");
-  Serial.print(AI2);
-  Serial.print(" AI3 ");
-  Serial.print(AI3);
-  Serial.print(" AI4 ");
-  Serial.println(AI4);*/
-  
-  /*Serial.print("DO1 ");
-  Serial.print(DO1);
-  Serial.print(" DO2 ");
-  Serial.print(DO2);
-  Serial.print(" DO3 ");
-  Serial.print(DO3);
-  Serial.print(" DO4 ");
-  Serial.print(DO4);
-  Serial.print(" DO5 ");
-  Serial.print(DO5);
-  Serial.print(" DO6 ");
-  Serial.print(DO6);
-  Serial.print(" DO7 ");
-  Serial.print(DO7);
-  Serial.print(" DO8 ");
-  Serial.println(DO8);*/
-
-  /*Serial.print("NTC1 ");
-  Serial.print(NTC1);
-  Serial.print(" NTC2 ");
-  Serial.print(NTC2);
-  Serial.print(" NTC3 ");
-  Serial.print(NTC3);
-  Serial.print(" NTC4 ");
-  Serial.print(NTC4);
-  Serial.print(" NTC5 ");
-  Serial.print(NTC5);
-  Serial.print(" NTC6 ");
-  Serial.print(NTC6);
-  Serial.print(" NTC7 ");
-  Serial.print(NTC7);
-  Serial.print(" NTC8 ");
-  Serial.println(NTC8);*/
-
   
  /////////////////////////////////////////////  
   Register();
